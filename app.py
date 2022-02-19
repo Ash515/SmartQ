acd0b37ebc5775616d07461952a665f317793169
Ash515 <aaashwin515@gmail.com>
2022-02-19 22:43:42 +0530

    Added code

--- app.py
+++ app.py
@@ -6,52 +6,52 @@ class Canteen:
         print(self.cname)
 
 
-class Dishes:
-    def __init__(self) -> None:
-        pass
+# class Dishes:
+#     def __init__(self) -> None:
+#         pass
 
-    def title(self):
-        print("\n***Dishes Catelogue***")
+#     def title(self):
+#         print("\n***Dishes Catelogue***")
 
-    def VegList(self, i1, i2, i3, i4, i5):
-        print("\n****1.Available Veg Foods****")
-        print(i1)
-        print(i2)
-        print(i3)
-        print(i4)
-        print(i5)
+#     def VegList(self, i1, i2, i3, i4, i5):
+#         print("\n****1.Available Veg Foods****")
+#         print(i1)
+#         print(i2)
+#         print(i3)
+#         print(i4)
+#         print(i5)
 
-    def NonVegList(self, i1, i2, i3, i4, i5):
-        print("\n ****2.Available NonVeg Foods****")
-        print(i1)
-        print(i2)
-        print(i3)
-        print(i4)
-        print(i5)
+#     def NonVegList(self, i1, i2, i3, i4, i5):
+#         print("\n ****2.Available NonVeg Foods****")
+#         print(i1)
+#         print(i2)
+#         print(i3)
+#         print(i4)
+#         print(i5)
 
-    def JuiceList(self, i1, i2, i3, i4, i5):
-        print("\n ****3.Available Juices****")
-        print(i1)
-        print(i2)
-        print(i3)
-        print(i4)
-        print(i5)
-
-
-class UserChoice(Dishes):
-    def __init__(self, choice):
-        self.choice = choice
-
-    def ChoiceCall(self):
-        if (self.choice == 1):
-            Dishes.VegList()
-        elif(self.choice == 2):
-            Dishes.NonVegList()
-        else:
-            Dishes.JuiceList()
+#     def JuiceList(self, i1, i2, i3, i4, i5):
+#         print("\n ****3.Available Juices****")
+#         print(i1)
+#         print(i2)
+#         print(i3)
+#         print(i4)
+#         print(i5)
 
 
-class ManagerDishEnteryChoice(Dishes):
+# class UserChoice(Dishes):
+#     def __init__(self, choice):
+#         self.choice = choice
+
+#     def ChoiceCall(self):
+#         if (self.choice == 1):
+#             Dishes.VegList()
+#         elif(self.choice == 2):
+#             Dishes.NonVegList()
+#         else:
+#             Dishes.JuiceList()
+
+
+class ManagerDishEnteryChoice():
     def __init__(self, choice):
         self.choice = choice
 
@@ -63,39 +63,44 @@ class ManagerDishEnteryChoice(Dishes):
         if self.choice == 1:
 
             i = 0
-            while (i <= itemCount):
+            while (i < itemCount):
                 item1 = input("Enter Item 1:")
-                item1Amt = int(input("Enter the amount for", item1))
+                item1Amt = int(input("Enter the amount for item1"))
                 vegList[item1] = item1Amt
                 i += 1
         elif(self.choice == 2):
 
             i = 0
-            while (i <= itemCount):
+            while (i < itemCount):
                 item2 = input("Enter Item 2:")
-                item2Amt = int(input("Enter the amount for", item2))
+                item2Amt = int(input("Enter the amount for item2"))
                 nonvegList[item2] = item2Amt
                 i += 1
         else:
 
             i = 0
-            while (i <= itemCount):
+            while (i < itemCount):
                 item3 = input("Enter Item 3:")
-                item3Amt = int(input("Enter the amount for", item3))
+                item3Amt = int(input("Enter the amount for item3"))
                 juiceList[item3] = item3Amt
                 i += 1
+        print("\n***Dishes***")
+        for k,v in vegList.items:
+            print("Item Name:",k,"Item Amount:",v)
+        for k,v in nonvegList.items:
+            print("Item Name:",k,"Item Amount:",v)
+        for k,v in juiceList.items:
+            print("Item Name:",k,"Item Amount:",v)
+        
 
 
 # Canteen Manager Interface
 nameEntry = input("Enter the canteen name:")
-Cname = Canteen("\n", nameEntry)
+Cname = Canteen(nameEntry)
 Cname.display()
-dishes = Dishes()
-dishes.title()
-
 status = int(input("Do you want to entry the new Items 1.Yes  2.No"))
 if status == 1:
-    item = input("1.Veg 2.Non Veg 3.Juice")
+    item = int(input("1.Veg 2.Non Veg 3.Juice"))
     if item == 1:
         vegentry = ManagerDishEnteryChoice(1)
         itemCnt = int(input("How many Items:"))
@@ -105,7 +110,7 @@ if status == 1:
     else:
         juiceentry = ManagerDishEnteryChoice(3)
 else:
-    pass
+    print("not selected ")
 
 
 # dishes.VegList("1.Idly","2.Dosa","3.Sambar","4.Lemon Rice","5.Veg rice")
